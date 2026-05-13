cd4edda064734f4f6b2612df541f63972afe7fc7
guillaumepeixotoduarte <guillaume.peixotoduarte@gmail.com>
2026-05-13 12:31:22 +0200

    Affichage des produits dans la page détails

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/product/{id}', name: 'app_product')]
+    public function index(int $id, ProductRepository $productRepository): Response
     {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Produit non trouvé');
+        }
+
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
+            'product' => $product,
         ]);
     }
 }
